9b55c34925a64cab60e03879c78ee7054c90564f
John Kryspin <johnkryspin@gmail.com>
2020-02-26 19:23:21 -0500

    better functions and readability

--- index.js
+++ index.js
@@ -9,16 +9,12 @@ function apexjs(code) {
 			{ name: "Username", type: "string", value: username },
 			{ name: "Platform", type: "string", value: platform }
 		]);
-		return new Promise((resolve, reject) => {
-			connectToAPI(username, platform, apiKey, (body, err) => {
-				if (err) reject("Player not found");
-				let data = body.data;
-				let character = {};
-				character.level = data.metadata.level;
-				character.legend_name = data.children[0].metadata.legend_name;
-				character.platformUserHandle = data.metadata.platformUserHandle;
-				resolve(character);
-			});
+		return connectToAPI(username, platform, apiKey).then((data) => {
+			let character = {};
+			character.level = data.metadata.level;
+			character.legend_name = data.children[0].metadata.legend_name;
+			character.platformUserHandle = data.metadata.platformUserHandle;
+			return character;
 		});
 	}
 	function getDetailedPlayer(username, platform) {
@@ -26,58 +22,62 @@ function apexjs(code) {
 			{ name: "Username", type: "string", value: username },
 			{ name: "Platform", type: "string", value: platform }
 		]);
-		return new Promise((resolve, reject) => {
-			connectToAPI(username, platform, apiKey, (body, err) => {
-				if (err) reject("Player not found");
-				let data = body.data;
-				resolve(data);
-			});
-		});
+		return connectToAPI(username, platform, apiKey)
+
 	}
 	return { getPlayer, getDetailedPlayer };
 }
 function getURL(username, platform) {
-	if (platform == "PC") {
-		return "/apex/v1/standard/profile/" + "5/" + username;
-	} else if (platform == "XBOX") {
-		return "/apex/v1/standard/profile/" + "1/" + username;
-	} else if (platform == "PSN") {
-		return "/apex/v1/standard/profile/" + "2/" + username;
-	} else {
-		throw Error("Platform must be PC, XBOX, or PSN");
+	switch (platform) {
+		case "PC":
+			return formatURL("5", username)
+		case "XBOX":
+			return formatURL("1", username)
+		case "PSN":
+			return formatURL("2", username)
+		default:
+			throw new Error("Platform must be PC, XBOX, or PSN");
+
 	}
 }
-function connectToAPI(username, platform, apiKey, callback) {
+function formatURL(number, username) {
+	return "/apex/v1/standard/profile/" + number + "/" + username;
+}
+function connectToAPI(username, platform, apiKey) {
 	let url = getURL(username, platform);
 
-	const options = {
-		host: apexAPIURL,
-		port: 443,
-		method: "GET",
-		path: url,
-		headers: {
-			"Content-Type": "application/json",
-			"TRN-Api-Key": apiKey
-		}
-	};
-	const req = https.request(options, res => {
-		let body = "";
-		res.on("data", function(chunk) {
-			body += chunk;
+	return new Promise((resolve, reject) => {
+
+		const options = {
+			host: apexAPIURL,
+			port: 443,
+			method: "GET",
+			path: url,
+			headers: {
+				"Content-Type": "application/json",
+				"TRN-Api-Key": apiKey
+			}
+		};
+		const req = https.request(options, res => {
+			let body = "";
+			res.on("data", function (chunk) {
+				body += chunk;
+			});
+			res.on("end", () => {
+				body = JSON.parse(body);
+				resolve(body.data)
+			});
 		});
-		res.on("end", () => {
-			body = JSON.parse(body);
-			callback(body, null);
+		req.on("error", error => {
+			console.error(error);
+			reject(error)
 		});
-	});
-	req.on("error", error => {
-		console.error(error);
-		callback(null, error);
-	});
-	req.end();
+		req.end();
+	})
+
 }
 function checkInput(inputs) {
-	for (input of inputs) {
+	for (const input of inputs) {
 		if (typeof input.value !== input.type)
 			throw TypeError(input.name + " must be a " + input.type);
 	}
